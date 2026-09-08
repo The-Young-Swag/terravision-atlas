fbf15f786a3543d453170bb04888adc1e1f624f1
the-young-swag <xplizit.nm7@gmail.com>
2026-09-08 20:21:46 +0000

    chore: remove react-router-dom wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './ui/styles/tailwind.css';
 import './ui/styles/globals.css';
@@ -13,8 +12,6 @@ if (!rootElement) {
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
